28620d4a97cb0dbd4cfb42abead16012cc7ce78e
peltriau <michel.peltriaux@vermkv.rlp.de>
2019-06-27 14:37:40 +0200

    Implementation
    * add case insensitivity to filter topics/organizations script

--- useroperations/static/useroperations/js/filter.js
+++ useroperations/static/useroperations/js/filter.js
@@ -5,12 +5,12 @@ $(document).ready(function(){
     */
     $(document).on("input", ".tile-filter-input", function(){
         var elem = $(this);
-        var val = elem.val();
+        var val = elem.val().toUpperCase();
         var elems = $(".tile");
         elems.each(function(i, elem){
             var obj = $(elem);
-            var conTitle = obj.find(".tile-header").attr("data-name");
-            var conDescr = obj.find(".tile-content-abstract-text").text().trim();
+            var conTitle = obj.find(".tile-header").attr("data-name").toUpperCase();
+            var conDescr = obj.find(".tile-content-abstract-text").text().trim().toUpperCase();
             if(conTitle.includes(val) || conDescr.includes(val)){
                 obj.show();
             }else{
