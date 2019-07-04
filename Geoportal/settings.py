a074eda16f3f071f60b07cd5fa655bdcddbc4fb7
peltriau <michel.peltriaux@vermkv.rlp.de>
2019-07-04 07:25:16 +0200

    Refactoring
    * forgot settings.py

--- Geoportal/settings.py
+++ Geoportal/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from django.utils.translation import gettext_lazy as _
 from django.utils.log import DEFAULT_LOGGING
-from Geoportal.utils.get_config_values import get_mapbender_config_value
+from Geoportal.utils.mbConfReader import get_mapbender_config_value
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
