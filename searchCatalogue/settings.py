0c2cecbf45c44b4dc7aafc9334eca55be43eca92
SvenTUM <s.crombrugge@tum.de>
2019-09-25 14:06:06 +0200

    Added missing states

--- searchCatalogue/settings.py
+++ searchCatalogue/settings.py
@@ -15,19 +15,19 @@ PROXIES = {
 iso3166_folder = "iso3166States/"
 __DE_DE = iso3166_folder + "wappen_DE.png"
 __DE_BW = iso3166_folder + "wappen_DE-BW.png"
-#__DE_BY = iso3166_folder + "wappen_DE-BY.png"
+__DE_BY = iso3166_folder + "wappen_DE-BY.png"
 __DE_BE = iso3166_folder + "wappen_DE-BE.png"
 __DE_BB = iso3166_folder + "wappen_DE-BB.png"
 __DE_HB = iso3166_folder + "wappen_DE-HB.png"
 __DE_HE = iso3166_folder + "wappen_DE-HE.png"
 __DE_HH = iso3166_folder + "wappen_DE-HH.png"
-#__DE_MV = iso3166_folder + "wappen_DE-MV.png"
+__DE_MV = iso3166_folder + "wappen_DE-MV.png"
 __DE_NI = iso3166_folder + "wappen_DE-NI.png"
 __DE_NW = iso3166_folder + "wappen_DE-NW.png"
 __DE_RP = iso3166_folder + "wappen_DE-RP.png"
 __DE_SL = iso3166_folder + "wappen_DE-SL.png"
 __DE_SN = iso3166_folder + "wappen_DE-SN.png"
-#__DE_ST = iso3166_folder + "wappen_DE-ST.png"
+__DE_ST = iso3166_folder + "wappen_DE-ST.png"
 __DE_SH = iso3166_folder + "wappen_DE-SH.png"
 __DE_TH = iso3166_folder + "wappen_DE-TH.png"
 
@@ -43,9 +43,9 @@ ISO3166_FILES = {
     "DE-BW": __DE_BW,
     "BW": __DE_BW,
 
-    # "Bayern": __DE_BY,
-    # "DE-BY": __DE_BY,
-    # "BY": __DE_BY,
+    "Bayern": __DE_BY,
+    "DE-BY": __DE_BY,
+    "BY": __DE_BY,
 
     "Berlin": __DE_BE,
     "DE-BE": __DE_BE,
@@ -67,9 +67,9 @@ ISO3166_FILES = {
     "DE-HE": __DE_HE,
     "HE": __DE_HE,
 
-    # "Mecklenburg-Vorpommern": __DE_MV,
-    # "DE-MV": __DE_MV,
-    # "MV": __DE_MV,
+    "Mecklenburg-Vorpommern": __DE_MV,
+    "DE-MV": __DE_MV,
+    "MV": __DE_MV,
 
     "Niedersachsen": __DE_NI,
     "DE-NI": __DE_NI,
@@ -92,9 +92,9 @@ ISO3166_FILES = {
     "DE-SN": __DE_SN,
     "SN": __DE_SN,
 
-    # "Sachsen-Anhalt": __DE_ST,
-    # "DE-ST": __DE_ST,
-    # "ST": __DE_ST,
+    "Sachsen-Anhalt": __DE_ST,
+    "DE-ST": __DE_ST,
+    "ST": __DE_ST,
 
     "Schleswig-Holstein": __DE_SH,
     "DE-SH": __DE_SH,
