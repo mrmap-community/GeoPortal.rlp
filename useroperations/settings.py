4ed91c16037882970b34de335ec34350f504a8d0
peltriau <michel.peltriaux@vermkv.rlp.de>
2019-12-16 14:49:08 +0100

    View options for applications
    * adds translations
    * adds listable view for applications/organizations/categories
    * adds useroperations/settings.py
      * adds constant for DEFAULT_ORDER and if DEFAULT_LIST_OR_NOT

---
+++ useroperations/settings.py
@@ -0,0 +1,13 @@
+"""
+Author: Michel Peltriaux
+Organization: Spatial data infrastructure Rhineland-Palatinate, Germany
+Contact: michel.peltriaux@vermkv.rlp.de
+Created on: 16.12.19
+
+"""
+
+# Refers to the listable view of applications/organizations/categories
+LISTED_VIEW_AS_DEFAULT = True
+
+# Refers to the default order of applications/organizations/categories
+ORDER_BY_DEFAULT = "rank"  # rank | title
--- useroperations/static/useroperations/js/filter.js
+++ useroperations/static/useroperations/js/filter.js
@@ -1,6 +1,6 @@
 
 
-function orderOrganizationTiles(orderBy){
+function orderTiles(orderBy){
     tiles = $(".tile-wrapper .tile");
     if(orderBy == "rank"){
         tiles.sort(function(a, b){
@@ -24,7 +24,70 @@ function orderOrganizationTiles(orderBy){
 }
 
 
+function toggleListView(toggleAsList){
+    var wrapper = $(".tile-wrapper");
+    var tiles = wrapper.find(".tile");
+
+    if(toggleAsList){
+        wrapper.addClass("tile-list-wrapper");
+        //tiles.addClass("tile-list-element")
+        // as list
+        tiles.each(function(i, element){
+            elem = $(element);
+            var title = elem.find(".tile-title");
+            var abstract = elem.find(".tile-content-abstract");
+
+            // hide non abstract elements
+            abstract.children().each(function(i, elem){
+                elem = $(elem);
+                if(!elem.hasClass("tile-content-abstract-text")){
+                    elem.hide();
+                }
+            });
+
+            var img = elem.find(".tile-header-img");
+            img.insertBefore(title);
+            abstract.insertAfter(title);
+        });
+
+    }else{
+        wrapper.removeClass("tile-list-wrapper");
+        tiles.removeClass("tile-list-element")
+        // as tiles
+        tiles.each(function(i, element){
+            elem = $(element);
+            var title = elem.find(".tile-title");
+            var abstract = elem.find(".tile-content-abstract");
+
+            // show non abstract elements
+            abstract.children().each(function(i, elem){
+                elem = $(elem);
+                if(!elem.hasClass("tile-content-abstract-text")){
+                    elem.show();
+                }
+            });
+
+            var content = elem.find(".tile-content");
+            var img = elem.find(".tile-header-img");
+            img.insertAfter(title);
+            abstract.appendTo(content);
+        });
+
+
+    }
+
+
+}
+
 $(document).ready(function(){
+
+    $(".tile-wrapper").ready(function(){
+        // make sure the order is being refreshed after a page reload
+        var tileOrderBy = $(".tile-filter-order").change();
+        var listViewTogglerVal = $(".switch input").is(":checked");
+        toggleListView(listViewTogglerVal);
+    });
+
     /*
     * Filter tiles
     */
@@ -32,17 +95,21 @@ $(document).ready(function(){
         var elem = $(this);
         var val = elem.val().toUpperCase();
         var elems = $(".tile");
+
         elems.each(function(i, elem){
             var obj = $(elem);
             var conTitle = obj.find(".tile-header").attr("data-name").toUpperCase();
             var conDescr = obj.find(".tile-content-abstract-text").text().trim().toUpperCase();
+
             // check if there is a responsible organization as well
             var respOrg = obj.find(".organization-title");
+
             if (respOrg !== null){
                 respOrg = respOrg.text().trim().toUpperCase()
             }else{
                 respOrg = "";
             }
+
             if(conTitle.includes(val) || conDescr.includes(val) || respOrg.includes(val)){
                 obj.show();
             }else{
@@ -56,9 +123,12 @@ $(document).ready(function(){
         var val = elem.val();
         var tileWrapper = $(".tile-wrapper");
 
-        if(tileWrapper.hasClass("organizations")){
-            tileWrapper.html(orderOrganizationTiles(val));
-        }
+        tileWrapper.html(orderTiles(val));
 
+    });
+
+    $(".switch input").on("change", function(){
+        var elem = $(this);
+        toggleListView(elem.is(":checked"));
     });
 });
