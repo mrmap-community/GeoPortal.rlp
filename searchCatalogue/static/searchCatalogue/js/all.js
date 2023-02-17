a02d312f5c3554676480e12f98b485de92d597b6
Armin Retterath <armin.retterath@googlemail.com>
2023-02-17 12:38:48 +0100

    Add files via upload
    Fix wrong tag handling

--- searchCatalogue/static/searchCatalogue/js/all.js
+++ searchCatalogue/static/searchCatalogue/js/all.js
@@ -733,7 +733,7 @@ $(document).ready(function() {
         // get terms from search input field
         var searchField = $(".simple-search-field");
         var terms = searchField.val();
-
+        //alert("new search terms: " + JSON.stringify(terms));
         // Make sure terms are set before search starts!
         // Racing condition might occur, when page is not completely loaded and the value of searchbarBackup
         // has not been pasted in the searchbar, yet. Check this in here!
@@ -1147,6 +1147,10 @@ $(document).ready(function() {
                 if(textStatus === "timeout"){
                     alert("The catalogue provider didn't respond. Please try again later.");
                 }
+		console.log(value);
+                console.log(jqXHR);
+                    console.log(textStatus);
+                    console.log(errorThrown);
                 /*else{
                     alert(errorThrown);
                 }
@@ -1381,8 +1385,16 @@ $(document).ready(function() {
     jQuery(document).on("click", ".-js-keyword", function() {
         var $self = jQuery(this);
         var keyword = $self.text().trim();
+	//alert($self.attr('data-params'));
+        var new_params = $self.attr('data-params').replace('searchText=&', '');
+        var params = new Proxy(new URLSearchParams(new_params), {
+            get: (searchParams, prop) => searchParams.get(prop),
+        });
+        let searchText = params.searchText;
+        //alert(searchText);
         var searchInput = $(".simple-search-field");
-        searchInput.val(keyword);
+        //searchInput.val(keyword);
+        searchInput.val(searchText);
         search.setParam("terms", keyword);
         prepareAndSearch();
     });
@@ -1506,11 +1518,15 @@ $(document).ready(function() {
     jQuery(document).on("click", ".-js-term", function() {
         var $this = jQuery(this);
         var text = $this.text().trim();
-
+        
         // remove search word from input field
         var searchField = $(".simple-search-field");
         var searchText = searchField.val().trim();
-        var searchTextArr = searchText.split(" ");
+	if (searchText.includes(',')) {
+            var searchTextArr = searchText.split(",");
+	} else {
+            var searchTextArr = searchText.split(" ");
+	}
         var searchTextArrNew = []
         $.each(searchTextArr, function(i, elem){
             if (elem.trim() != text){
